3782fa8343402cbaa8417f20fabdca1bf98266c1
Mario Hernandez <mariohernandezv@yahoo.com.ar>
2014-10-21 21:20:07 -0500

    registro de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,25 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
-	  def new
-	  	@user = User.new
-	  end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Bienvenido a la Sample App!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+
+	def new
+		@user = User.new
+	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:name, :email, 
+				:password, :password_confirmation)
+		end
 end
